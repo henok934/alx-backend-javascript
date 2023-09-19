ad4fcc51caf274c469bf76239e93d115e88ea6c0
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-09-19 14:47:05 -0400

    0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,4 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ * @author Teklemariam Mossie <https://github.com/henok934>
+ */
 function displayMessage(message) {
   console.log(message);
 }
+
 module.exports = displayMessage;
