4ba19420dbb358a37bc7217b122e497ea1b03c6f
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-09-19 15:29:29 -0400

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,29 @@
-/**
- * Displays a message on the STDOUT.
- * @param {String} data The data to display.
- * @author Teklemariam Mossie <https://github.com/henok934>
- */
+// 1-stdin.js
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
+// Check if input is received from command line argument
+if (process.argv[2]) {
+  const name = process.argv[2];
   process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write('This important software is now closing\n');
+} else {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
+    process.stdout.write(`Your name is: ${name}\n`);
 
-  // Check if the user wants to exit
-  if (name.toLowerCase() === 'exit') {
-    process.stdout.write('This important software is now closing\n');
+    // Check if the user wants to exit
+    if (name.toLowerCase() === 'exit') {
+      process.stdout.write('This important software is now closing\n');
+      process.exit();
+    } else {
+      process.stdout.write('What is your name?\n');
+    }
+  });
+
+  // Handle program termination
+  process.on('SIGINT', () => {
+    process.stdout.write('\nThis important software is now closing\n');
     process.exit();
-  } else {
-    process.stdout.write('What is your name?\n');
-  }
-});
-
-// Handle program termination
-process.on('SIGINT', () => {
-  process.stdout.write('\nThis important software is now closing\n');
-  process.exit();
-});
+  });
+}
