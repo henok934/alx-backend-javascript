de781552054318ba6fb26b4cf0e4a77504912f59
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-09-20 10:56:39 -0400

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -10,7 +10,11 @@ console.log('Welcome to Holberton School, what is your name?');
 rl.on('line', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
+  
+  rl.close(); // Close the readline interface after capturing user input
+});
 
+rl.on('close', () => {
   if (!process.stdin.isTTY) {
     console.log('This important software is now closing');
     process.exit(0);
