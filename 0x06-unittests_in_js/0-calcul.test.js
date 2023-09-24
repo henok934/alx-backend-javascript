ae85c9982c979bbf73db914ca5fc00ac11d6fe23
teklemariammossie1@gmail.com <teklemariammossie1@gmail.com>
2023-09-24 06:44:29 -0400

    unittest in js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('should return the correct sum for 4.2 and 2.6', function () {
+describe('calculateNumber', () => {
+  it('should return the correct sum for 4.2 and 2.6', () => {
     assert.strictEqual(calculateNumber(4.2, 2.6), 7);
   });
 
-  it('should return the correct sum for 4.2 and 7.6', function () {
+  it('should return the correct sum for 4.2 and 7.6', () => {
     assert.strictEqual(calculateNumber(4.2, 7.6), 12);
   });
 });
